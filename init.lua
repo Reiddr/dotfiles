c525dd1bdefd6da785140cba8dcc21a77019ae61
Dylan <dylan@seedspider.com>
2025-10-09 09:07:05 +1300

    add some key remaps

--- init.lua
+++ init.lua
@@ -1,6 +1,10 @@
-vim.g.mapleader = ' '
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.cursorlineopt = 'line'
 vim.opt.path = vim.opt.path + "**"
-vim.keymap.set('n', '<leader>m', ':!ctags -R<CR><CR>')
+vim.g.mapleader = ' '
+vim.api.nvim_set_keymap('n', '<leader>m', ':!ctags -R<CR><CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>w', ':w<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>q', ':q<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>s', ':so<CR>', {noremap = true, silent = true})
+

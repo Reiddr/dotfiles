001a3594cb65eb61a38a94c4b7ae2c261e0f5aa9
Dylan <dylan@seedspider.com>
2025-10-28 14:32:40 +1300

    add ghostty config and update tmux config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,6 +4,10 @@ vim.opt.cursorlineopt = 'line'
 vim.opt.path = vim.opt.path + "**"
 vim.opt.termguicolors = true
 
+vim.cmd("command! -nargs=1 -complete=help H :enew | set buftype=help | :h <args>")
+
+-- vim.treesitter.start()
+
 vim.g.mapleader = ' '
 vim.api.nvim_set_keymap('n', '<leader>m', ':!ctags -R<CR><CR>', {noremap = true, silent = true})
 vim.api.nvim_set_keymap('n', '<leader>w', ':w<CR>', {noremap = true, silent = true})

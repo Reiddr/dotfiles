2742001b5204d02c74d86045b2d2acfe4c3d2151
Dylan <dylan@seedspider.com>
2026-03-17 13:58:09 +1300

    add some more convenience functions

--- .bashrc.d/sway_func.sh
+++ .bashrc.d/sway_func.sh
@@ -1,17 +1,41 @@
 #!/bin/bash
-print_bar () {
+clipval () {
 	local i=$1
-	if [[ $i -gt 100 ]]; then 
-		i=100
+	if [[ $i -lt $2 ]]; then 
+		i=$2
 	fi
-	if [[ $i -lt 0 ]]; then
-		i=0
+	if [[ $i -gt $3 ]]; then
+		i=$3
 	fi
-	printf "i: %d" "$i"
+	echo $i
+}
+
+getpercent() {
+	local n=$(clipval $1 $2 $3)
+	local p=$(( ($n - $2) * 100 / ($3 - $2) ))
+	echo $p
+}
+
+printbar () {
+	local fill="#"
+	local empty="."
+	local n=$(clipval $1 0 100)
+	local f=$((n/5))
+	printf "n: %d f: %d\n" "$n" "$f"
+	printf "["
+	for i in {1..20}; do
+		if [[ i -le f ]]; then 
+			printf "$fill"
+		else
+			printf "$empty"
+		fi
+	done
+	printf "]"
 }
 
 adj_bar () {
-	printf "(q)uit\n"
+	
+	printf "(q)uit --(h) (l)++ \n"
 	while true; do
 		read -n1 -s key
 		if [[ $key == 'q' ]]; then
